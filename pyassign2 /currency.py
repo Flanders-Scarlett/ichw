072b1720e680318892a04cf7908e0bbcd271b6a9
Fu Yixuan <43433467+Flanders-Scarlett@users.noreply.github.com>
2018-12-03 00:01:46 +0800

    Update currency.py

--- pyassign2 /currency.py
+++ pyassign2 /currency.py
@@ -2,27 +2,27 @@ def exchange(currency_from, currency_to, amount_from):
     #定义函数 currency_from为输入的已有的货币类型，用三位大写英文字母表示的 currency_to为想转化的货币类型#
             #amount_from为想转化的货币个数#
 
-    from urllib.request import urlopen
+    from urllib.request import urlopen 
     
     doc = urlopen('http://cs1110.cs.cornell.edu/2016fa/a1server.php?from='+
                   currency_from+'&to='+currency_to+'&amt='+amount_from)
     docstr = doc.read()
     doc.close()
     jstr = docstr.decode('ascii')
-    return (jstr)  #输出的jstr为string类型哦#
+    return (jstr)  #输出的jstr为string类型#
 
-def live(jstr): #该函数为将上述jstr转化为字典，并判断输入是否有误啦#
+def live(jstr): #该函数为将上述jstr转化为字典，并判断输入是否有误#
     import json
-    jstr = json.loads(jstr)
+    jstr = json.loads(jstr) #该函数为将str转化为dict#
     to = jstr['to']
-    if jstr['success'] ==False:
+    if jstr['success'] ==False: #判断函数输入时是否有误#
         return('出错了QAQ，错误类别为'+jstr['error'])
     else:
         return(love(to))
            
-def love(to): #该函数是将‘2.333 USD’转化为2.333浮点型哦#
+def love(to): #该函数是将‘2.333 USD’转化为2.333浮点型#
     num = ''
-    for eachChar in to:
+    for eachChar in to: #遍历字符串to#
         if eachChar in '1234567890.':
             num = num + eachChar
     return(float(num))
